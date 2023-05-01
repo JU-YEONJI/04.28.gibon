50169f8aec751ed09048649b13b632f2276d9e90
JU-YEONJI <juji1007@naver.com>
2023-05-01 10:49:14 +0900

    end

--- js/index9.js
+++ js/index9.js
@@ -6,3 +6,15 @@ The aabbccddeeffgg
 aaa@naver.com
 abc@gmail.com
 `
+
+console.log(str.match(/\w/g))
+console.log(str.match(/\b/g))
+console.log(str.match(/\d/g))
+console.log(str.match(/\b\w{1,}/))
+const num = `             the   wold   hello   !`
+console.log(num.match(/\s/g));
+console.log(num.replace(/\s/g,''));
+
+const sub = 'abc@gmail.com';
+console.log(sub.match(/(?<=@).{1,}/g))
+console.log(sub.match(/.{1,}(?=@)/g))
